e2956b60128fc63dc62d344152cc6af1253df0de
Dinuka Rathnayake <it22140616@my.sliit.lk>
2026-02-22 14:51:46 +0530

    Added product model

---
+++ src/main/java/com/sliit/product_service/entity/Product.java
@@ -0,0 +1,49 @@
+package com.sliit.product_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;  
+    private String name;
+    private Double price;
+    public Product() {}
+
+    // Parameterized Constructor
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
